dc19637da0a7593922375b5d70af5a150ef05c17
Natasa90 <154846657+Natasa90@users.noreply.github.com>
2024-04-17 12:28:11 +0200

    Delete CodeWars directory

--- CodeWars/blankPages.js
+++
@@ -1,8 +0,0 @@
-function blankPages (n, m) { 
-    if (n < 0 | m < 0) {
-        return 0; 
-    } else 
-        return n * m ; 
-}
-
-console.log(blankPages(5,5));
--- CodeWars/noSpaceFunction.js
+++
@@ -1,5 +0,0 @@
-function noSpace(x) {
-    return x.split(" ").join("")
-}
-
-console.log (noSpace("Today is going to be a rainy day."));
